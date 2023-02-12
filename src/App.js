b35e9d6bcbfb01f1403a179823736c40c366c0c8
AmanWorku <amexworku2014@gmail.com>
2023-02-12 18:27:53 +0300

    Add basic files in the components and pages folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Details from './pages/Details';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App" />
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path=":details" element={<Details />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
