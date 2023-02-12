c4bae7e76266fcb6590cee89f7ba53298f23d884
AmanWorku <amexworku2014@gmail.com>
2023-02-12 22:09:41 +0300

    Add a sample data to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,43 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className="heading-content">
+        Home
+      </div>
+      <div className="contents">
+        <div className="cont content1">
+          content 1
+        </div>
+        <div className="cont content2">
+          content 2
+        </div>
+        <div className="cont content3">
+          content 3
+        </div>
+        <div className="cont content4">
+          content 4
+        </div>
+        <div className="cont content5">
+          content 5
+        </div>
+        <div className="cont content5">
+          content 6
+        </div>
+        <div className="cont content5">
+          content 7
+        </div>
+        <div className="cont content5">
+          content 8
+        </div>
+        <div className="cont content5">
+          content 9
+        </div>
+        <div className="cont content5">
+          content 10
+        </div>
+      </div>
+    </>
   );
 }
 
