b7d63d5de1b31b16a8dbfebb808692ce909b8ac5
AmanWorku <amexworku2014@gmail.com>
2023-02-13 08:32:01 +0300

    Create a thunk function for retrieving data and created reducer

--- src/redux/Home/home.js
+++ src/redux/Home/home.js
@@ -1,12 +1,36 @@
+import { createReducer, createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import BaseURL from '../services/countryAPI';
 
-// const FETCH_COUNTRIES = 'FETCH_COUNTRIES';
-
+const FETCH_COUNTRIES = 'FETCH_COUNTRIES';
+const INITIAL_STATE = [];
 const fetchCountries = async () => {
   const data = await (await fetch(BaseURL)).json();
   return data;
 };
 
-fetchCountries();
+export const retrieveCountries = createAsyncThunk(FETCH_COUNTRIES,
+  async (obj, thunkAPI) => {
+    const currentState = thunkAPI.getState();
+    if (currentState.countriesReducer.length === 0) {
+      const response = await fetchCountries();
+      const countriesData = [];
+      response.forEach((country) => {
+        const aCountry = {
+          id: country.alpha3code,
+          name: country.name,
+          populations: country.population,
+        };
+        countriesData.push(aCountry);
+      });
+      return countriesData;
+    }
+    return currentState.countriesReducer;
+  });
 
-export default fetchCountries;
+const countries = createReducer(INITIAL_STATE, ((builder) => {
+  builder
+    .addCase(retrieveCountries.fulfilled, ((state, action) => action.payload))
+    .addDefaultCase(((state) => [...state]));
+}));
+
+export default countries;
