962406c36339a64bf99b43b6dd74b04d5125c442
AmanWorku <amexworku2014@gmail.com>
2023-02-13 17:13:27 +0300

    Add a link in the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Country from '../components/Country';
 import { retrieveCountries } from '../redux/Home/home';
+import Details from './Details';
 
 function Home() {
   const dispatch = useDispatch();
@@ -11,13 +13,15 @@ function Home() {
 
   const countries = useSelector((state) => state.countriesReducer);
   const renderCountries = (cou) => cou.map((country) => (
-    <Country
-      key={country.id}
-      id={country.id}
-      name={country.name}
-      population={country.population}
-      flag={country.flag}
-    />
+    <Link className="details-link" to={`/details/${country.id}`} key={country.id} element={<Details />}>
+      <Country
+        key={country.id}
+        id={country.id}
+        name={country.name}
+        population={country.population}
+        flag={country.flag}
+      />
+    </Link>
   ));
   return (
     <>
