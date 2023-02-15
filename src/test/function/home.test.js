175605aeaef0b9fc1b4b6a9e257d7fbb4bd452f6
AmanWorku <amexworku2014@gmail.com>
2023-02-15 18:33:56 +0300

    Add test for the home page and country

--- src/test/function/home.test.js
+++ src/test/function/home.test.js
@@ -1,8 +1,12 @@
-import { fetchCountries } from '../../redux/Home/home';
+import { fetchCountries, retrieveCountries } from '../../redux/Home/home';
 
 describe('Countries data', () => {
   test('fetch correct data', async () => {
     const data = await fetchCountries();
+    expect(data.length).not.toBeNull();
+  });
+  test('retrieve correct data', async () => {
+    const data = await retrieveCountries();
     expect(data.length).not.toBeNull();
   });
 });
